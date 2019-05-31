26d488cd86d988afe7ca4c52f7ae8118d3e28bdf
David Zamora <davidzamorarey@gmail.com>
2019-05-31 14:10:31 +0200

    Heroes project created

---
+++ 02-spa/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+// Routes
+import { APP_ROUTING } from './app.routes';
+
+// Services
+
+// Components
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTING
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
