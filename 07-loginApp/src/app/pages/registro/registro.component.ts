8d768ef67e18738a858fa418bd603c422c70c4a9
David Zamora <davidzamorarey@gmail.com>
2019-06-13 11:53:48 +0200

    AuthApp finished and LoginApp project started

---
+++ 07-loginApp/src/app/pages/registro/registro.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from '../../models/usuario.model';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  usuario: UsuarioModel;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.usuario = new UsuarioModel();
+
+    this.usuario.email = 'davidzamorarey@gmail.com';
+   }
+
+  onSubmit() {
+    console.log('Formulario enviado');
+    console.log(this.usuario);
+  }
+
+}
